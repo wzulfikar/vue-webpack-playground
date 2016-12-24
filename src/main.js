b0194e0583dca69c522741b36fcc260d4a20d95e
wzulfikar-MBA <wzulfikar@ymail.com>
2016-12-24 15:17:11 +0800

    stuck. tutorial from scotch mismatch

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
+import Hello from './components/Hello.vue'
+// import Home from './components/Home.vue'
+
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+// We want to apply VueResource and VueRouter
+// to our Vue instance
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+// Pointing routes to the components they should use
+const routes = [
+  {path: '/hello', component: Hello},
+  {path: '/', component: App},
+
+  // Any invalid route will redirect to home
+  {path: '*', component: '/'}
+]
+
+const router = new VueRouter({
+  routes
+})
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  router
+}).$mount('#app')
